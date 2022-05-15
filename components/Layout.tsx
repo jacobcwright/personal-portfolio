8977b4ecdb46b36412ba5e86c0e498fa4ce6987c
Jacob Wright <jacobwright@Jacobs-MacBook-Air.local>
2022-05-14 23:00:56 -0600

    tailwind not working.. cringe

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
+  <>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
@@ -21,7 +21,7 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
     </header>
     {children}
     <Footer />
-  </div>
+  </>
 );
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-const Home = () => <Layout title="About"></Layout>;
-
+const Home = () => {
+  return (
+    <Layout title="Jacob Wright">
+      <div className="h-full w-full flex flex-row justify-between bg-black text-red-600">
+        <div className="h-full w-1/2 bg-black">comp 1</div>
+        <div>comp 2 (threejs comp?)</div>
+      </div>
+    </Layout>
+  );
+};
 export default Home;
