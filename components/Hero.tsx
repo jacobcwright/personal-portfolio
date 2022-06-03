9a1d757547d9ae032d58453926c6d7161bddf866
jacobcwright <jacob.wright.ut@gmail.com>
2022-06-02 22:11:28 -0600

    adjusted home page picture and color

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { HiOutlineEye } from "react-icons/Hi";
-import Pfp from "../public/img/block.gif";
+import Pfp from "../public/img/linkedin.png";
 
 function Hero() {
   return (
