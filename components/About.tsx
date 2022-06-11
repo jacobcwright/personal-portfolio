1cb89cfbb9ab48f44533f1bb8eb7190a28e4bc72
jacobcwright <jacob.wright.ut@gmail.com>
2022-06-10 20:28:23 -0600

    basic wording

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function About() {
-  return <div id="about">About</div>;
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>
+        My name is Jacob Wright. I am a geek who loves to create things! My
+        passion is doing all that I can to improve my life and the lives of
+        those around me.
+      </p>
+      <p>
+        I work as a software engineer at GuideCX. I am a computer science senior
+        at BYU, and will be participating in BYU Sandbox 2022, a startup
+        incubator program.
+      </p>
+    </div>
+  );
 }
 
 export default About;
