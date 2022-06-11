813296abb595ec2e1144770ba017ccc5e213c653
jacobcwright <jacob.wright.ut@gmail.com>
2022-06-11 09:07:30 -0600

    added implement me message on contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -99,6 +99,11 @@ function ContactPage() {
             <button
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => {
+                alert(
+                  "Thank you for your message!\n I will implement this as soon as possible"
+                );
+              }}
             >
               Send Message
             </button>
