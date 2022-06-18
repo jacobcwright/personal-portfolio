a066b47afc31768fb4de43b6c16f505232c2aa87
jacobcwright <jacob.wright.ut@gmail.com>
2022-06-18 15:14:21 -0600

    added favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import Navbar from "./Navbar";
+import Image from "next/image";
 
 type Props = {
   children?: ReactNode;
@@ -12,6 +13,7 @@ const Layout = ({ children, title }: Props) => (
   <>
     <Head>
       <title>{title}</title>
+      <link rel="icon" href="/img/jw.png"></link>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
